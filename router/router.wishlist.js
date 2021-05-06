5eb02ef29cd7c92526e509abc0d093101c1c8a9a
Sumanbitw <007sumanghosh@gmail.com>
2021-05-06 20:40:45 +0530

    backend

--- router/router.wishlist.js
+++ router/router.wishlist.js
@@ -11,6 +11,7 @@ router.get("/", async ( req,res ) => {
     }  
 })
 
+//post
 router.post("/", async (req,res) => {
     try {
         const savedWishlist = await wishlistModels.findOneAndUpdate(
